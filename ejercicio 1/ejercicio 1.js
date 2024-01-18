163b2d8d3751227c69599b1a2082c6d4cb7f757d
jdllanghi <146900717+jdllanghi@users.noreply.github.com>
2024-01-18 17:17:31 -0500

    correcciones

--- ejercicio 1/ejercicio 1.js
+++ ejercicio 1/ejercicio 1.js
@@ -1,3 +1,9 @@
+'use strict'
+
+// Ingresar un nombre con promp y verificar si el nombre ingresado se encuentra en el
+//vector
+
+
 let students = ["Juan", "Jose", "Pedro", "Maria", "Alejandra"];
 let nombreIngresado = prompt("Ingrese un nombre:");
 if (students.includes(nombreIngresado)) {
